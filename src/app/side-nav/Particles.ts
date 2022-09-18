ea8246a99dbbed78824b90e9b9945250115c6f1d
T-Al-D <tahaaldailami2010@hotmail.de>
2022-09-18 20:39:53 +0200

    colorful mouse on router-module

---
+++ src/app/side-nav/Particles.ts
@@ -0,0 +1,44 @@
+export class Particle {
+    x: number;
+    y: number;
+    size: number;
+    speedX: number;
+    speedY: number;
+    sizeDecrease: number;
+    hueColor = 0;
+    colorjump = 3;
+
+    constructor(
+        xPos: number,
+        yPos: number,
+        size: number,
+        speedX: number,
+        speedY: number,
+        sizeDecrease: number
+    ) {
+        this.x = xPos;
+        this.y = yPos;
+        this.size = size;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.sizeDecrease = sizeDecrease;
+        this.hueColor += this.colorjump;
+    }
+    // new position for Particles
+    update() {
+        if (this.size > 0.5) {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.size -= this.sizeDecrease;
+            this.hueColor += this.colorjump;
+        }
+    }
+
+    // draw the Particles on the canvas
+    draw(canvasContext: CanvasRenderingContext2D) {
+        // hue, saturation, lightness
+        const hue: string = "hsl(" + this.hueColor + ", 65%, 50%)";
+        canvasContext.strokeStyle = hue;
+        canvasContext.strokeRect(this.x, this.y, this.size, this.size);
+    }
+}
