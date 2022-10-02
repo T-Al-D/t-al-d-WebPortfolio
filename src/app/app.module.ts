412c54f9fd4ccab23d09c6e7bfa5609f1e0cd278
T-Al-D <tahaaldailami2010@hotmail.de>
2022-10-02 19:12:39 +0200

    put examples in projects for later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FooterComponent } from "./footer/footer.component";
 import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { SideNavComponent } from "./side-nav/side-nav.component";
 import { MainRouterComponent } from "./main-router/main-router.component";
-//import { GamesComponent } from "./have-fun/games/games.component";
 
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -17,6 +16,7 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
 import { MatRippleModule } from "@angular/material/core";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
     declarations: [
@@ -26,7 +26,6 @@ import { MatRippleModule } from "@angular/material/core";
         FooterComponent,
         SideNavComponent,
         MainRouterComponent,
-        //GamesComponent,
     ],
     imports: [
         BrowserModule,
@@ -39,6 +38,7 @@ import { MatRippleModule } from "@angular/material/core";
         MatIconModule,
         MatListModule,
         MatRippleModule,
+        MatCardModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
